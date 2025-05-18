a8c8d91d68f0fb5e0a1e48c19e0c5812bff313d9
Oliver Midbrink <oliver.midbrink@hotmail.com>
2025-05-18 20:41:28 +0200

    Added a test change for testing PR autobuild purposes (working variant)

--- Ivy/Auth/AuthOption.cs
+++ Ivy/Auth/AuthOption.cs
@@ -3,7 +3,7 @@
 namespace Ivy.Auth;
 
 public record AuthOption(AuthFlow Flow, string? Name = null, string? Id = null, Icons? Icon = null);
-
+// Comment here for PR auto check purposes - working variant
 public enum AuthFlow
 {
     EmailPassword,
