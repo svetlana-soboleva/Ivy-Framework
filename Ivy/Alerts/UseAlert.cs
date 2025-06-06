5ef1a7e673d19a7780768e9564234877d44996a1
nielsbosma <niels@bosmainteractive.se>
2025-06-06 17:07:01 +0200

    Finished fix for Alerts.

--- Ivy/Alerts/UseAlert.cs
+++ Ivy/Alerts/UseAlert.cs
@@ -1,30 +1,49 @@
+using Ivy.Client;
 using Ivy.Core;
 using Ivy.Core.Hooks;
 
 namespace Ivy.Alerts;
 
-public delegate void ShowAlertDelegate(string message, string? title = null, AlertButtonSet buttonSet = AlertButtonSet.OkCancel);
+public delegate void ShowAlertDelegate(string message, Action<AlertResult> callback, string? title = null, AlertButtonSet buttonSet = AlertButtonSet.OkCancel);
 
 public static class UseAlertExtensions
 {
-    public static (IView? alertView, AlertResult alertResult, ShowAlertDelegate showAlert) UseAlert<TW>(this TW view) where TW : ViewBase =>
+    public static (IView? alertView, ShowAlertDelegate showAlert) UseAlert<TW>(this TW view) where TW : ViewBase =>
         view.Context.UseAlert();
     
-    public static (IView? alertView, AlertResult alertResult, ShowAlertDelegate showAlert) UseAlert(this IViewContext context)
+    public static (IView? alertView, ShowAlertDelegate showAlert) UseAlert(this IViewContext context)
     {
         var alertResult = context.UseState(AlertResult.Undecided);
         var isOpen = context.UseState(false);
-        var alertOptions = context.UseState((AlertOptions)null!);
+        var alertOptions = context.UseState<AlertOptions?>();
+        var alertCallback = context.UseState<Action<AlertResult>?>();
+        var client = context.UseService<IClientProvider>();
+        
+        context.UseEffect(() =>
+        {
+            if(alertResult.Value != AlertResult.Undecided && alertCallback.Value != null)
+            {
+                try
+                {
+                    alertCallback.Value(alertResult.Value);
+                }
+                catch (Exception ex)
+                {
+                    client.Toast(ex);
+                }
+            }
+        }, [alertResult, alertCallback]);
 
         var view = isOpen.Value ? new AlertView(alertResult, isOpen, alertOptions.Value) : null;
          
-        var showAlert = new ShowAlertDelegate((message, title, buttonSet) =>
+        var showAlert = new ShowAlertDelegate((message, callback, title, buttonSet) =>
         {
-            alertOptions.Set(new AlertOptions(message, title, buttonSet));
+            alertOptions.Set(new AlertOptions(title, message, buttonSet));
             alertResult.Set(AlertResult.Undecided);
+            alertCallback.Set(callback);
             isOpen.Set(true);
         });
         
-        return (view, alertResult.Value, showAlert);
+        return (view, showAlert);
     }
 }
