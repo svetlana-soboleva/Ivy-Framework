ba8b0b588421af382d40fc04c8812c1059bd929d
rorychatt <pavel.rinne@gmail.com>
2025-07-27 22:00:05 +0200

    chore: test

--- frontend/src/lib/textBlockClassMap.ts
+++ frontend/src/lib/textBlockClassMap.ts
@@ -4,7 +4,7 @@ export const textBlockClassMap: Record<string, string> = {
   H2: 'scroll-m-20 text-3xl font-semibold tracking-tight mt-8 mb-4',
   H3: 'scroll-m-20 text-2xl font-semibold tracking-tight mt-4 mb-4',
   H4: 'scroll-m-20 text-xl font-semibold tracking-tight mt-4 mb-4',
-  P: 'scroll-m-20 text-md leading-6',
+  P: 'scroll-m-20 text-md leading-6 mb-4',
   Lead: 'text-xl text-muted-foreground',
   Blockquote: 'border-l-2 pl-6 italic mb-2',
   InlineCode:
@@ -14,7 +14,7 @@ export const textBlockClassMap: Record<string, string> = {
   h2: 'scroll-m-20 text-3xl font-semibold tracking-tight mt-8 mb-4',
   h3: 'scroll-m-20 text-2xl font-semibold tracking-tight mt-4 mb-4',
   h4: 'scroll-m-20 text-xl font-semibold tracking-tight mt-4 mb-4',
-  p: 'scroll-m-20 text-md leading-6',
+  p: 'scroll-m-20 text-md leading-6 mb-4',
   strong: 'font-semibold',
   em: 'italic',
   ul: 'ml-6 list-disc [&>li:first-child]:mt-0 mb-2',
