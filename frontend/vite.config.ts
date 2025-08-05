29436d93aee92317e1f9fb68b122969e53d17b6e
rorychatt <pavel.rinne@gmail.com>
2025-08-05 14:03:58 +0200

    chore: cleanup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -60,9 +60,6 @@ export default defineConfig(({ mode }) => ({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  server: {
-    historyApiFallback: true,
-  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
