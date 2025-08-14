a5dfa837bc6af099aad8f79acd5ef585bdef8c68
rorychatt <pavel.rinne@gmail.com>
2025-08-14 14:34:17 +0200

    Revert "bug: sketchy fix for empty nodes"
    This reverts commit 9a043c7a0d35369d5c8d6ed091cac80753ac28b3.

--- frontend/src/hooks/use-backend.tsx
+++ frontend/src/hooks/use-backend.tsx
@@ -44,10 +44,7 @@ const widgetTreeToXml = (node: WidgetNode) => {
   }
   let childrenXml = '';
   if (node.children && node.children.length > 0) {
-    childrenXml = node.children
-      .filter(child => child != null)
-      .map(child => widgetTreeToXml(child))
-      .join('');
+    childrenXml = node.children.map(child => widgetTreeToXml(child)).join('');
     return `<${tagName} ${attributes.join(' ')}>${childrenXml}</${tagName}>`;
   } else {
     return `<${tagName} ${attributes.join(' ')} />`;
