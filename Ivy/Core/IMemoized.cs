f3ba26466f7439725b2487fc2a4c084b9442e7e7
Artem Khvorostianyi <ar.khvorostianyi@gmail.com>
2025-08-23 19:38:16 +0300

    chore: IMemoized.cs doc

--- Ivy/Core/IMemoized.cs
+++ Ivy/Core/IMemoized.cs
@@ -1,6 +1,17 @@
 namespace Ivy.Core;
 
+/// <summary>
+/// Defines a contract for objects that support memoization by providing
+/// their current state values for caching and optimization purposes.
+/// </summary>
 public interface IMemoized
 {
+    /// <summary>
+    /// Retrieves an array of values that represent the current state
+    /// of the implementing object. These values are used for memoization
+    /// to determine if the object's state has changed and whether
+    /// cached results can be reused.
+    /// </summary>
+    /// <returns>An array of objects representing the current state values for memoization.</returns>
     public object[] GetMemoValues();
 }
