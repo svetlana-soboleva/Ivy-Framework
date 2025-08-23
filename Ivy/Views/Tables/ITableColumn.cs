af65988c5a7684e95941d5be10c3b78cd97b45a8
rorychatt <pavel.rinne@gmail.com>
2025-08-23 17:58:42 +0200

    chore: ITableColumn docs

--- Ivy/Views/Tables/ITableColumn.cs
+++ Ivy/Views/Tables/ITableColumn.cs
@@ -1,6 +1,15 @@
 namespace Ivy.Views.Tables;
 
+/// <summary>
+/// Interface for table columns that can generate header and data cells from model records.
+/// </summary>
+/// <typeparam name="TModel">The type of data model for the table rows.</typeparam>
 public interface ITableColumn<in TModel>
 {
+    /// <summary>
+    /// Builds the column header and data cells for the given records.
+    /// </summary>
+    /// <param name="records">The data records to generate cells from.</param>
+    /// <returns>Tuple containing the header cell and array of data cells.</returns>
     public (TableCell header, TableCell[] cells) Build(IEnumerable<TModel> records);
 }
