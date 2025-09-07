6226ffb6966e3db27067e1dc4c6e98ca7863c6ae
Artem Khvorostianyi <ar.khvorostianyi@gmail.com>
2025-09-08 00:37:02 +0300

    feat: implement better scrollbar handling

--- frontend/src/components/ui/resizable.tsx
+++ frontend/src/components/ui/resizable.tsx
@@ -33,7 +33,7 @@ const ResizableHandle = ({
     {...props}
   >
     {withHandle && (
-      <div className="z-10 flex h-4 w-3 items-center justify-center rounded-sm border bg-border">
+      <div className="z-10 flex h-4 w-3 items-center justify-center rounded-sm border bg-border opacity-0 hover:opacity-100 transition-opacity">
         <GripVertical className="h-2.5 w-2.5" />
       </div>
     )}
