1b5d746d3727c52d9784990275959abe04d04fa4
rorychatt <pavel.rinne@gmail.com>
2025-09-08 11:00:37 +0200

    chore: better scroll logic for header layout

--- frontend/src/widgets/layouts/HeaderLayoutWidget.tsx
+++ frontend/src/widgets/layouts/HeaderLayoutWidget.tsx
@@ -28,13 +28,13 @@ export const HeaderLayoutWidget: React.FC<HeaderLayoutWidgetProps> = ({
 
   return (
     <div
-      className="remove-ancestor-padding flex flex-col w-full"
+      className="remove-ancestor-padding flex flex-col w-full h-full"
       style={styles}
     >
       <div className="flex-none p-2 border-b bg-background w-full">
         {slots.Header}
       </div>
-      <div className="flex-1 min-h-0 w-full">
+      <div className="flex-1 min-h-0 w-full overflow-hidden">
         <ScrollArea className="h-full w-full">
           <div className="p-4 w-full">{slots.Content}</div>
         </ScrollArea>
