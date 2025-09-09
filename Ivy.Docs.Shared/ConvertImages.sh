c1c779e66327d0970e144b1d01efe2d50a9260e6
Zach Wolfe <zachrwolfe@me.com>
2025-09-09 09:20:55 -0500

    Make ConvertImages.sh not care about working directory

--- Ivy.Docs.Shared/ConvertImages.sh
+++ Ivy.Docs.Shared/ConvertImages.sh
@@ -2,9 +2,11 @@
 
 # WARNING: This script replaces all pngs in Assets with webps.
 
-cd Assets
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ASSETS_DIR="$SCRIPT_DIR/Assets"
+
 shopt -s nullglob          # makes *.png expand to nothing if there are no matches
-for f in *.png; do
+for f in "$ASSETS_DIR"/*.png; do
     webp="${f%.png}.webp"
     magick "$f" -strip -quality 80 "$webp" && rm "$f"
 done
