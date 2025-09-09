ffecef8214aa94cd4e7a49786b16f053b92c7afd
rorychatt <pavel.rinne@gmail.com>
2025-09-09 13:26:13 +0200

    chore: xml docs polishing

--- Ivy/Widgets/WidgetBase.cs
+++ Ivy/Widgets/WidgetBase.cs
@@ -4,93 +4,64 @@ using Ivy.Shared;
 // ReSharper disable once CheckNamespace
 namespace Ivy;
 
-/// <summary>
-/// Base class for all widgets.
-/// </summary>
+/// <summary>Base class for all widgets.</summary>
 public abstract record WidgetBase<T> : AbstractWidget where T : WidgetBase<T>
 {
-    /// <summary>
-    /// Constructor for the widget.
-    /// </summary>
-    /// <param name="children">The children of the widget.</param>
+    /// <summary>Constructor for the widget.</summary>
     protected WidgetBase(params object[] children) : base(children)
     {
     }
 
-    /// <summary>
-    /// The width of the widget.
-    /// </summary>
+    /// <summary>The width of the widget.</summary>
     [Prop] public Size? Width { get; set; }
 
-    /// <summary>
-    /// The height of the widget.
-    /// </summary>
+    /// <summary>The height of the widget.</summary>
     [Prop] public Size? Height { get; set; }
 
-    /// <summary>
-    /// Whether the widget is visible.
-    /// </summary>
+    /// <summary>Whether the widget is visible.</summary>
     [Prop] public bool Visible { get; set; } = true;
 
-    /// <summary>
-    /// The test ID of the widget. 
-    /// Used for finding the widget in the DOM for testing.
-    /// </summary>
+    /// <summary>The test ID of the widget. Used for finding the widget in the DOM for testing.</summary>
     [Prop] public string? TestId { get; set; }
 }
 
 public static class WidgetBaseExtensions
 {
-    /// <summary>
-    /// Set the width of the widget.
-    /// </summary>
+    /// <summary>Set the width of the widget.</summary>
     /// <param name="widget">The widget to set the width of.</param>
     /// <param name="width">The width of the widget.</param>
-    /// <returns>The widget with the width set.</returns>
     public static T Width<T>(this T widget, Size? width) where T : WidgetBase<T>
     {
         return widget with { Width = width };
     }
 
-    /// <summary>
-    /// Set the width of the widget.
-    /// </summary>
+    /// <summary>Set the width of the widget.</summary>
     /// <param name="widget">The widget to set the width of.</param>
     /// <param name="units">The width of the widget in units.</param>
-    /// <returns>The widget with the width set.</returns>
     public static T Width<T>(this T widget, int units) where T : WidgetBase<T>
     {
         return widget with { Width = Shared.Size.Units(units) };
     }
 
-    /// <summary>
-    /// Set the width of the widget.
-    /// </summary>
+    /// <summary>Set the width of the widget.</summary>
     /// <param name="widget">The widget to set the width of.</param>
     /// <param name="units">The width of the widget in units.</param>
-    /// <returns>The widget with the width set.</returns>
     public static T Width<T>(this T widget, float units) where T : WidgetBase<T>
     {
         return widget with { Width = Shared.Size.Fraction(units) };
     }
 
-    /// <summary>
-    /// Set the width of the widget.
-    /// </summary>
+    /// <summary>Set the width of the widget.</summary>
     /// <param name="widget">The widget to set the width of.</param>
     /// <param name="units">The width of the widget in units.</param>
-    /// <returns>The widget with the width set.</returns>
     public static T Width<T>(this T widget, double units) where T : WidgetBase<T>
     {
         return widget with { Width = Shared.Size.Fraction(Convert.ToSingle(units)) };
     }
 
-    /// <summary>
-    /// Set the width of the widget.
-    /// </summary>
+    /// <summary>Set the width of the widget.</summary>
     /// <param name="widget">The widget to set the width of.</param>
     /// <param name="percent">The width of the widget in percent.</param>
-    /// <returns>The widget with the width set.</returns>
     public static T Width<T>(this T widget, string percent) where T : WidgetBase<T>
     {
         if (percent.EndsWith("%"))
@@ -101,56 +72,41 @@ public static class WidgetBaseExtensions
         throw new ArgumentException("Invalid percentage value.");
     }
 
-    /// <summary>
-    /// Set the height of the widget.
-    /// </summary>
+    /// <summary>Set the height of the widget.</summary>
     /// <param name="widget">The widget to set the height of.</param>
     /// <param name="height">The height of the widget.</param>
-    /// <returns>The widget with the height set.</returns>
     public static T Height<T>(this T widget, Size? height) where T : WidgetBase<T>
     {
         return widget with { Height = height };
     }
 
-    /// <summary>
-    /// Set the height of the widget.
-    /// </summary>
+    /// <summary>Set the height of the widget.</summary>
     /// <param name="widget">The widget to set the height of.</param>
     /// <param name="units">The height of the widget in units.</param>
-    /// <returns>The widget with the height set.</returns>
     public static T Height<T>(this T widget, int units) where T : WidgetBase<T>
     {
         return widget with { Height = Shared.Size.Units(units) };
     }
 
-    /// <summary>
-    /// Set the height of the widget.
-    /// </summary>
+    /// <summary>Set the height of the widget.</summary>
     /// <param name="widget">The widget to set the height of.</param>
     /// <param name="units">The height of the widget in units.</param>
-    /// <returns>The widget with the height set.</returns>
     public static T Height<T>(this T widget, float units) where T : WidgetBase<T>
     {
         return widget with { Height = Shared.Size.Fraction(units) };
     }
 
-    /// <summary>
-    /// Set the height of the widget.
-    /// </summary>
+    /// <summary>Set the height of the widget.</summary>
     /// <param name="widget">The widget to set the height of.</param>
     /// <param name="units">The height of the widget in units.</param>
-    /// <returns>The widget with the height set.</returns>
     public static T Height<T>(this T widget, double units) where T : WidgetBase<T>
     {
         return widget with { Height = Shared.Size.Fraction(Convert.ToSingle(units)) };
     }
 
-    /// <summary>
-    /// Set the height of the widget.
-    /// </summary>
+    /// <summary>Set the height of the widget.</summary>
     /// <param name="widget">The widget to set the height of.</param>
     /// <param name="percent">The height of the widget in percent.</param>
-    /// <returns>The widget with the height set.</returns>
     public static T Height<T>(this T widget, string percent) where T : WidgetBase<T>
     {
         if (percent.EndsWith("%"))
@@ -161,56 +117,41 @@ public static class WidgetBaseExtensions
         throw new ArgumentException("Invalid percentage value.");
     }
 
-    /// <summary>
-    /// Set the size of the widget.
-    /// </summary>
+    /// <summary>Set the size of the widget.</summary>
     /// <param name="widget">The widget to set the size of.</param>
     /// <param name="size">The size of the widget.</param>
-    /// <returns>The widget with the size set.</returns>
     public static T Size<T>(this T widget, Size? size) where T : WidgetBase<T>
     {
         return widget.Width(size).Height(size);
     }
 
-    /// <summary>
-    /// Set the size of the widget.
-    /// </summary>
+    /// <summary>Set the size of the widget.</summary>
     /// <param name="widget">The widget to set the size of.</param>
     /// <param name="units">The size of the widget in units.</param>
-    /// <returns>The widget with the size set.</returns>
     public static T Size<T>(this T widget, int units) where T : WidgetBase<T>
     {
         return widget.Width(units).Height(units);
     }
 
-    /// <summary>
-    /// Set the size of the widget.
-    /// </summary>
+    /// <summary>Set the size of the widget.</summary>
     /// <param name="widget">The widget to set the size of.</param>
     /// <param name="units">The size of the widget in units.</param>
-    /// <returns>The widget with the size set.</returns>
     public static T Size<T>(this T widget, float units) where T : WidgetBase<T>
     {
         return widget.Width(units).Height(units);
     }
 
-    /// <summary>
-    /// Set the size of the widget.
-    /// </summary>
+    /// <summary>Set the size of the widget.</summary>
     /// <param name="widget">The widget to set the size of.</param>
     /// <param name="units">The size of the widget in units.</param>
-    /// <returns>The widget with the size set.</returns>
     public static T Size<T>(this T widget, double units) where T : WidgetBase<T>
     {
         return widget.Width(units).Height(units);
     }
 
-    /// <summary>
-    /// Set the size of the widget.
-    /// </summary>
+    /// <summary>Set the size of the widget.</summary>
     /// <param name="widget">The widget to set the size of.</param>
     /// <param name="percent">The size of the widget in percent.</param>
-    /// <returns>The widget with the size set.</returns>
     public static T Size<T>(this T widget, string percent) where T : WidgetBase<T>
     {
         if (percent.EndsWith("%"))
@@ -224,43 +165,31 @@ public static class WidgetBaseExtensions
         throw new ArgumentException("Invalid percentage value.");
     }
 
-    /// <summary>
-    /// Set the visibility of the widget.
-    /// </summary>
+    /// <summary>Set the visibility of the widget.</summary>
     /// <param name="widget">The widget to set the visibility of.</param>
     /// <param name="visible">Whether the widget is visible.</param>
-    /// <returns>The widget with the visibility set.</returns>
     public static T Visible<T>(this T widget, bool visible = true) where T : WidgetBase<T>
     {
         return widget with { Visible = visible };
     }
 
-    /// <summary>
-    /// Show the widget.
-    /// </summary>
+    /// <summary>Show the widget.</summary>
     /// <param name="widget">The widget to show.</param>
-    /// <returns>The widget with the visibility set.</returns>
     public static T Show<T>(this T widget) where T : WidgetBase<T>
     {
         return widget with { Visible = true };
     }
 
-    /// <summary>
-    /// Hide the widget.
-    /// </summary>
+    /// <summary>Hide the widget.</summary>
     /// <param name="widget">The widget to hide.</param>
-    /// <returns>The widget with the visibility set.</returns>
     public static T Hide<T>(this T widget) where T : WidgetBase<T>
     {
         return widget with { Visible = false };
     }
 
-    /// <summary>
-    /// Set the test ID of the widget.
-    /// </summary>
+    /// <summary>Set the test ID of the widget.</summary>
     /// <param name="widget">The widget to set the test ID of.</param>
     /// <param name="testId">The test ID of the widget.</param>
-    /// <returns>The widget with the test ID set.</returns>
     public static T TestId<T>(this T widget, string testId) where T : WidgetBase<T>
     {
         return widget with { TestId = testId };
