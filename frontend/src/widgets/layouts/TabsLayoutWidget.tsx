ef0dea689105eead0b6dfca997a711a49f2782d2
Mikael Rinne <40919111+rorychatt@users.noreply.github.com>
2025-09-20 19:02:00 +0200

    Revert "(ui/ux): (tabs): better align toggle button"

--- frontend/src/widgets/layouts/TabsLayoutWidget.tsx
+++ frontend/src/widgets/layouts/TabsLayoutWidget.tsx
@@ -856,8 +856,7 @@ export const TabsLayoutWidget = ({
               <TabsList
                 ref={tabsListRef}
                 useRadix={useRadix}
-                className="relative h-auto w-full gap-0.5 bg-transparent p-0 flex justify-start flex-nowrap"
-                style={{ marginTop: '16px' }}
+                className="relative h-auto w-full gap-0.5 mt-2.5 bg-transparent p-0 flex justify-start flex-nowrap"
               >
                 {orderedTabWidgets.map(tabWidget => {
                   if (!React.isValidElement(tabWidget)) return null;
