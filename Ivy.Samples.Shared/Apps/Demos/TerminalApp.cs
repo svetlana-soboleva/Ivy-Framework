62eec63cf3f9d255c8f12d8039c0a38918fc0caf
Artem Khvorostianyi <ar.khvorostianyi@gmail.com>
2025-10-01 00:05:00 +0300

    feat: remove example

--- Ivy.Samples.Shared/Apps/Demos/TerminalApp.cs
+++ Ivy.Samples.Shared/Apps/Demos/TerminalApp.cs
@@ -13,11 +13,6 @@ public class TerminalApp : ViewBase
                    .AddCommand("echo Hello, world!")
                    .AddOutput("Hello, world!")
                    .Title("Your first Ivy app!")
-                 | new Terminal()
-                   .AddCommand("ivy db add --provider postgres --connection-string \"...\" --name MyDb --use-default-schema!")
-                   .AddOutput("Hello, world!")
-                   .Title("Your first Ivy app!")
-
             ;
     }
 }
