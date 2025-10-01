8cf38f402edc4e8171d6c5d5cd1eba9f5aed9903
Jeel Rupareliya <jeelrupareliya69@gmail.com>
2025-10-01 21:26:56 +0530

    added back multiline words-break css

--- frontend/src/widgets/details/DetailWidget.tsx
+++ frontend/src/widgets/details/DetailWidget.tsx
@@ -32,8 +32,10 @@ export const DetailWidget: React.FC<DetailWidgetProps> = ({
       </div>
       <div
         className={cn(
-          'align-middle text-body min-w-0 whitespace-normal break-words',
-          multiLine ? 'text-left pb-3' : 'text-right ml-auto p-3 pl-2 pr-0'
+          'align-middle text-body min-w-0',
+          multiLine
+            ? 'whitespace-normal break-words text-left pb-3'
+            : 'truncate text-right ml-auto p-3 pl-2 pr-0'
         )}
       >
         {children}
