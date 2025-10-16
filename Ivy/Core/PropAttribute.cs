6155e695f525caa4a16a6f6e1644981e1610d345
Artem Khvorostianyi <ar.khvorostianyi@gmail.com>
2025-10-16 11:11:13 +0300

    feat: improve XML docs for Prop (#1119)

--- Ivy/Core/PropAttribute.cs
+++ Ivy/Core/PropAttribute.cs
@@ -1,13 +1,13 @@
 namespace Ivy.Core;
 
-/// <summary>Attribute marking properties as serializable widget properties included in widget's JSON representation supporting regular and attached properties.</summary>
-/// <param name="attached">Optional name of attached property. When specified, property becomes attached property set on child widgets and accessed by parent widget.</param>
+/// <summary>Serializes properties to widget JSON state (regular or attached).</summary>
+/// <param name="attached">Attached property name. When set, property is attached to children.</param>
 [AttributeUsage(AttributeTargets.Property)]
 public class PropAttribute(string? attached = null) : Attribute
 {
-    /// <summary>Name of attached property. When not null, indicates property is attached property set on child widgets.</summary>
+    /// <summary>Attached property name.</summary>
     public string? AttachedName { get; set; } = attached;
 
-    /// <summary>Whether this property is attached property used in parent-child widget relationships.</summary>
+    /// <summary>True if this is an attached property.</summary>
     public bool IsAttached => AttachedName != null;
 }
