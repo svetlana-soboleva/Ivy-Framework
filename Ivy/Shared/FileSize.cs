8ef46d54d4cef9440c5c232e0531b46a83cfaa80
nielsbosma <niels@bosmainteractive.se>
2025-11-06 10:18:01 +0100

    feat(shared): add FileSize and FileTypes utility classes
    Add static utility classes for file size conversions and common file MIME types.

---
+++ Ivy/Shared/FileSize.cs
@@ -0,0 +1,29 @@
+﻿namespace Ivy.Shared;
+
+public static class FileSize
+{
+    public static long FromBytes(double bytes)
+    {
+        return (long)bytes;
+    }
+
+    public static long FromKilobytes(double kilobytes)
+    {
+        return (long)(kilobytes * 1024);
+    }
+
+    public static long FromMegabytes(double megabytes)
+    {
+        return (long)(megabytes * 1024 * 1024);
+    }
+
+    public static long FromGigabytes(double gigabytes)
+    {
+        return (long)(gigabytes * 1024 * 1024 * 1024);
+    }
+
+    public static long FromTerabytes(double terabytes)
+    {
+        return (long)(terabytes * 1024 * 1024 * 1024 * 1024);
+    }
+}
---
+++ Ivy/Shared/FileTypes.cs
@@ -0,0 +1,7 @@
+﻿namespace Ivy.Shared;
+
+public static class FileTypes
+{
+    public static readonly string Text = "text/plain";
+    public static readonly string Pdf = "application/pdf";
+}
